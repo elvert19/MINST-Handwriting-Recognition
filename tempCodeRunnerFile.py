6bed9ce6006ed151b8d45291771548a672d555b0
elvert19 <gachuruelvis9@gmail.com>
2026-04-18 18:01:04 +0300

    refactor: remove environment and temp files for a cleaner repo

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-
-import sys, os, gzip, requests, time
-import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
-from matplotlib.animation import FuncAnimation
-from matplotlib.patches import FancyBboxPatch
-import threading
